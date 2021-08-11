65736b690ba206d31c2473fb243fef36f84f8e89
Juan Jaminton Yate <hamtino@gmail.com>
2021-08-11 14:20:51 -0500

    se crea metodo GET por id

--- src/routes/Person.js
+++ src/routes/Person.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
 const router = Router();
-import { createPerson, getPerson } from "../controller/person.controller"
+import { createPerson, getPerson, getOnePerson, deletePerson } from "../controller/person.controller"
 
 router.post('/', createPerson)
 router.get('/', getPerson)
+
+router.get('/:id', getOnePerson)
+
+router.get('/:id', getOnePerson)
+
+
 
 export default router;
