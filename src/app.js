7246d3bfe6519c3763176d26999a0cc0178f70ec
Juan Jaminton Yate <hamtino@gmail.com>
2021-08-11 09:21:09 -0500

    configuracion de la ruta principal

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import express from "express";
 import morgan from "morgan";
+//importar la ruta de Person
+import personRoutes from "./routes/Person"
 
+//inicializacion de framework express
 const app = express(); 
 
 // middlewares
 app.use(morgan("dev"));
 app.use(json());
+
+//rutas
+app.use("/api/person", personRoutes);
 
 export default app;
